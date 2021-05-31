0fec2af7fca9d72237b0fd11abcfae5f8ce291a0
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-01 10:57:33 +1200

    Add files via upload

---
+++ 91883-884 Asmt/Python_versions/Version_1 (user Details)/Version_1b.py
@@ -0,0 +1,21 @@
+#User Details
+#Using fuctions to greet the user
+def greet():
+    global name
+    while True:
+        name = input("What is your name? : ")
+        if name.isalpha():
+            break
+        print("Please enter only A-Z characters only")
+
+                  
+greet()
+print("Hello!",name)
+#Asking user to enter their age 
+def age():
+    while True:
+        age=input("How old are you? : ")
+        if age.isnumeric():
+            break
+        print("this is not a valid data type.")
+age()
---
+++ 91883-884 Asmt/Python_versions/Version_2 (instructions)/Version_2a.py
@@ -0,0 +1,6 @@
+#Asking user's if they want to read instructions
+rules = input("Do you want to read the rules{}:?: \na. Yes \nb. No \n=>")
+if rules == 'yes' or rules == 'y' or rules == 'a':
+    print("To play you will be ask how many rounds you would want to play. Once you have \nchosen your rounds (1-5) you will recive questions to answer.Every question you answer \ncorrect you will earn 1 point if you get any question incorrect you wont recive any points. Enjoy the game!")
+elif rule == 'no' or rule == 'x':
+    print("You may continue without the rules.")
