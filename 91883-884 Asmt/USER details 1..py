224a656cf018ca155762503add00ac5e7bac58ac
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-01 10:56:28 +1200

    Delete 91883-884 Asmt directory

--- 91883-884 Asmt/USER details 1..py
+++
@@ -1,8 +0,0 @@
-#ask name
-while True:
-    name = input("enter your name : ")
-    if name.isalpha():
-        break
-    print("please enter characters A-Z only")
-#ask age
-age = int(input("What is your age? : "))
