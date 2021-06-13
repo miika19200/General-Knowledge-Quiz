5040ef5c22707b2e28582d0487a6a982d2d35bee
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-14 10:39:39 +1200

    Update Version_1b.py

--- Python_versions/Version_1 (user Details)/Version_1b.py
+++ Python_versions/Version_1 (user Details)/Version_1b.py
@@ -3,7 +3,7 @@
 def greet():
     global name#so name outside of def can be used
     while True:
-        name = input("What is your name? : ")
+        name = input("What is your name? : ")#ask for users name
         if name.isalpha():#isaplha is used to track errors
             break#generates error
         print("Please enter only A-Z characters only")
@@ -14,7 +14,7 @@ print("Hello!",name)
 #Asking user to enter their age 
 def age():
     while True:
-        age=input("How old are you? : ")
+        age=input("How old are you? : ")#ask for users age
         if age.isnumeric():#isnumeric is used to track errors
             break#generates error
         print("this is not a valid data type.")
