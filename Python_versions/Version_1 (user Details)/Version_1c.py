7bcb8299541c6e489c6952415c46b18e18e28d0a
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-14 10:31:36 +1200

    Update Version_1c.py

--- Python_versions/Version_1 (user Details)/Version_1c.py
+++ Python_versions/Version_1 (user Details)/Version_1c.py
@@ -1,21 +1,21 @@
 #User Details
 #Using fuctions to greet the user
 def greet():
-    global name
+    global name#so name outside of def can be used
     while True:
         name = input("What is your name? : ")
-        if name.replace(' ','').isalpha():
-            break
+        if name.replace(' ','').isalpha():#isaplha is used to track errors
+            break#generates error
         print("Please enter only A-Z characters only")
 
                   
-greet()#calling_function
+greet()#calling function
 print("Hello!",name)
 #Asking user to enter their age 
 def age():
     while True:
         age=input("How old are you? : ")
-        if age.replace(' ','').isnumeric():
+        if age.replace(' ','').isnumeric():#isnumeric is used to track errors
             break
         print("this is not a valid data type.")
-age()#calling_function
+age()#calling function
