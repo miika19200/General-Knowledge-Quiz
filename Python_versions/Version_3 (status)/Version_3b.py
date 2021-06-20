c5e29802c6c157dd14de5cfcdd0b438e6fa0898a
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-21 11:12:01 +1200

    Update Version_3b.py

--- Python_versions/Version_3 (status)/Version_3b.py
+++ Python_versions/Version_3 (status)/Version_3b.py
@@ -1,7 +1,7 @@
 #asking user if they are ready to take the quiz
 ready=input("Are you ready to take the quiz?: Press y to continue or any other key to exit:").lower()#.lower is used to allow capitals by converting capitals into lowercase
 #using if function
-if ready == "y" or ready == "yes":#if user inputs your yes program continues 
+if ready == "y" or ready == "yes":#if user inputs y or yes program continues 
     print("Let's continue!")  
 #using else function
 else:#if user input different characters program ends
