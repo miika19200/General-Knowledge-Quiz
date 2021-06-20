638779004f7c65c968e1586cdf074c4c6c4612e4
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-20 14:53:22 +1200

    Update Version_1c.py

--- Python_versions/Version_1 (user Details)/Version_1c.py
+++ Python_versions/Version_1 (user Details)/Version_1c.py
@@ -4,7 +4,8 @@ def greet():#using def function so i can use mutiple times without repeating
     global name#so name outside of def can be used
     while True: #using while True loop
         name = input("What is your name? : ")#ask user to input their name
-        if name.replace(' ','').isalpha():#isaplha is used to track errors
+        #.replace allows spaces
+        if name.replace(' ','').isalpha():#isaplha is used to trap errors
             break#breaks loop
         print("Please enter only A-Z characters only")
 
@@ -15,7 +16,8 @@ print("Hello!",name)
 def age():#using def function so i can use mutiple times without repeating
     while True:#using while True loop
         age=input("How old are you? : ")#ask user to input their age
-        if age.replace(' ','').isnumeric():#isnumeric is used to track errors
+        #.replace allows spaces
+        if age.replace(' ','').isnumeric():#isnumeric is used to trap errors
             break#breaks loop
         print("this is not a valid data type, please enter only numbers.")
 age()#calling function
