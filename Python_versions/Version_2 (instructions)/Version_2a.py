2e66d49f7a55d78bb5e2da56e05dbf387d9a9900
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-21 11:06:19 +1200

    Update Version_2a.py

--- Python_versions/Version_2 (instructions)/Version_2a.py
+++ Python_versions/Version_2 (instructions)/Version_2a.py
@@ -1,6 +1,7 @@
 #Asking user's if they want to read instructions
 rules = input("Do you want to read the rules{}:?: \na. Yes \nb. No \n=>")#users option to choose if they want to read rules or not
-if rules == 'yes' or rules == 'y' or rules == 'a':#prints rule
-    print("To play you will be ask how many rounds you would want to play. Once you have \nchosen your rounds (1-5) you will recive questions to answer.Every question you answer \ncorrect you will earn 1 point if you get any question incorrect you wont recive any points. Enjoy the game!")
-elif rules == 'no' or rules == 'x':#continues with program
+if rules == 'yes' or rules == 'y' or rules == 'a':#Using if function
+    print("To play you will be ask how many rounds you would want to play. Once you have \nchosen your rounds (1-5) you will recive questions to answer.Every question you answer \ncorrect you will earn 1 point if you get any question incorrect you wont recive any points. Enjoy the game!")#printing rules
+elif rules == 'no' or rules == 'x':#Using elif functions
+    #continues with program
     print("You may continue without the rules.")
