7d1b2705c7a91aa81564db21b6630335583553b2
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-20 13:11:34 +1200

    Update Version_2c.py

--- Python_versions/Version_2 (instructions)/Version_2c.py
+++ Python_versions/Version_2 (instructions)/Version_2c.py
@@ -1,5 +1,5 @@
 #Asking user's if they want to read instructions
-def rules():
+def rules():#using def function so i can use mutiple times without repeating
     rules = input("Do you want to read the rules{}:?: if yes enter: a or Yes or yes or y,if no enter: b or No or n or N.  \na. Yes \nb. No \n=>")#ask user if they want to read rules or not
     if rules == 'yes' or rules == 'y' or rules == 'a' or rules == 'YeS':#prints rules
         print("To play you will be ask how many rounds you would want to play. Once you have \nchosen your rounds (1-5) you will recive questions to answer.Every question you answer \ncorrect you will earn 1 point if you get any question incorrect you wont recive any points. Enjoy the game!")
