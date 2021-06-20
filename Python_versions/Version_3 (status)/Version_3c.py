079f3627955b80cc11025bf9f0f4ddd7892e765f
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-20 13:28:25 +1200

    Update Version_3c.py

--- Python_versions/Version_3 (status)/Version_3c.py
+++ Python_versions/Version_3 (status)/Version_3c.py
@@ -1,9 +1,9 @@
 #asking user if they are ready to take the quiz
-def status():
-    ready=input("Are you ready to take the quiz?: Press y to continue or any other key to exit:").lower()
-    if ready == "y" or ready == "yes" :
+def status():#using def function so i can use mutiple times without repeating
+    ready=input("Are you ready to take the quiz?: Press y to continue or any other key to exit:").lower()#.lower is used to make capital into lowercase
+    if ready == "y" or ready == "yes" :#program continues 
         print("Let's continue!")  
-    else:
+    else:#ends program 
         print("See you later!")
-        exit()
-status()
+        exit()#exits program
+status()#calling function 
