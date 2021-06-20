622ccbf1c847371cd37c035125b3c599f742e384
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-20 13:12:21 +1200

    Update Version_7c.py

--- Python_versions/Version_7 (assembled)/Version_7c.py
+++ Python_versions/Version_7 (assembled)/Version_7c.py
@@ -29,9 +29,9 @@ print("********************Welcome to my GENERAL KNOWLEDGE QUIZ!****************
 #Asking user's if they want to read instructions
 def rules():#using def function so i can use mutiple times without repeating
     rules = input("Do you want to read the rules{}:? If yes enter: a or Yes or yes or y, If no enter: b or No or n or N. \na. Yes \nb. No \n=>")
-    if rules == 'yes' or rules == 'y' or rules == 'a':
+    if rules == 'yes' or rules == 'y' or rules == 'a':#prints rules
         print("To play you will be ask how many rounds you would want to play. Once you have \nchosen your rounds (1-5) you will recive questions to answer.Every question you answer \ncorrect you will earn 1 point if you get any question incorrect you wont recive any points. Enjoy the game!")
-    else:
+    else:#continues program
         print("You may continue without the rules.")
 
     
