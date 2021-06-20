b220e1fb9559f61de995fe97a55008f3a36f027f
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-21 10:59:30 +1200

    Update Version_1a.py

--- Python_versions/Version_1 (user Details)/Version_1a.py
+++ Python_versions/Version_1 (user Details)/Version_1a.py
@@ -1,9 +1,6 @@
 #User Details
-#Using fuctions to greet the user
 name = input("What is your name? : ")#ask user to input their name
-                      
-
 print("Hello!",name)#prints user name
-#Asking user to enter their age 
+
 age=input("How old are you? : ")#ask user to input their age
 
