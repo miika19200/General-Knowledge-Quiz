45064bc6d7ba98ab15b85ae7fa96627fc8c895db
miika19200 <78886191+miika19200@users.noreply.github.com>
2021-06-20 13:25:14 +1200

    Update Version_3b.py

--- Python_versions/Version_3 (status)/Version_3b.py
+++ Python_versions/Version_3 (status)/Version_3b.py
@@ -1,7 +1,7 @@
 #asking user if they are ready to take the quiz
-ready=input("Are you ready to take the quiz?: Press y to continue or any other key to exit:").lower()
-if ready == "y" or ready == "yes" :
+ready=input("Are you ready to take the quiz?: Press y to continue or any other key to exit:").lower()#.lower is used to make capital into lowercase
+if ready == "y" or ready == "yes" :#program continues 
     print("Let's continue!")  
-else:
+else:#ends program
     print("See you later!")
-    exit()
+    exit()#exits program
